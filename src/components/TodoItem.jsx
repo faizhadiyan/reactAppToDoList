5c3a160d86dd0ad89a38843476971d3311c45e09
Faiz <faizhadiyanfirza@gmail.com>
2023-05-25 16:14:12 +0700

    bikin fitur hapus berhasil

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const TodoItem = ({ todo, toggleCompleted }) => {
+const TodoItem = ({ todo, toggleCompleted, deleteTodo }) => {
 
   const getTodoTitleStyle = () => {
     if (todo.completed === true) {
@@ -10,35 +10,52 @@ const TodoItem = ({ todo, toggleCompleted }) => {
     }
   }
 
+  // const deleteTodo = () => {
+  //   if (todo.completed === true) {
+  //     return { textDecoration: 'line-through' }
+  //   } else {
+  //     return { textDecoration: 'none' }
+  //   }
+  // }
 
   return (
     <div style={styles.todoItem}>
         {/* Menambahkan checkbox */}
         <input  type="checkbox" 
                 style={styles.checkbox} 
-                onChange={() => toggleCompleted(todo.id)}/>
+                onChange={() => {toggleCompleted(todo.id)}}/>
 
         <p style={getTodoTitleStyle()}>{todo.title}</p>
+        <button style={styles.button} 
+        onClick={() => deleteTodo(todo.id)}>x</button>
     </div>
   )
 }
 
-const styles = {
-    todoItem: {
-      border: '2px solid #f4f4f4',
-      fontSize: '24px',
-      // Tambahkan styles di bawah ini
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-    },
 
-    // Tambahkan styles di bawah ini
-    checkbox: {
-      marginRight: '10px',
-      height: '18px',
-      width: '18px',
-    },
+const styles = {
+  todoItem: {
+    border: '2px solid #f4f4f4',
+    fontSize: '24px',
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: '0 20px',
+  },
+  checkbox: {
+    height: '18px',
+    width: '18px',
+  },
+  button: {
+    backgroundColor: '#BB0000',
+    color: '#fff',
+    height: '30px',
+    width: '30px',
+    borderRadius: '100%',
+    border: 'none',
+    cursor: 'pointer',
+    fontSize: '16px',
+  },
 }
 
 export default TodoItem
